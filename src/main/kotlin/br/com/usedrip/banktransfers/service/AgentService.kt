9c25e0aa096b56a7893528e0759610656bbff2b6
Andrelino <andregisagyn@gmail.com>
2023-08-18 03:03:54 -0300

    add new rule

--- src/main/kotlin/br/com/usedrip/banktransfers/service/AgentService.kt
+++ src/main/kotlin/br/com/usedrip/banktransfers/service/AgentService.kt
@@ -4,18 +4,26 @@ import br.com.usedrip.banktransfers.dto.TransferForm
 import br.com.usedrip.banktransfers.dto.TransfersDTO
 import br.com.usedrip.banktransfers.model.Account
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 @Service
 class AgentService(
     private val interService: InterTransferService,
     private val intraService: IntraTransferService,
+    private val customTransferService: CustonTransferService,
     private val accountService: AccountService
 ) {
     fun execute(transferForm: TransferForm) : TransfersDTO {
         val accountTo : Account = accountService.getAccountTo(transferForm.accountTo)
         val accountFor : Account = accountService.getAccountFor(transferForm.accountFor)
+        var transfersDTO : TransfersDTO;
         if (accountTo.bank?.code != accountFor.bank?.code)
-            return interService.convert(transferForm,accountTo,accountFor)
-        else return intraService.convert(transferForm,accountTo,accountFor)
+            transfersDTO =  interService.convert(transferForm,accountTo,accountFor)
+        else
+            if (transferForm.amount > BigDecimal.valueOf(2000.00))
+                transfersDTO = customTransferService.convert(transferForm,accountTo,accountFor)
+            else transfersDTO = intraService.convert(transferForm,accountTo,accountFor)
+
+        return transfersDTO;
     }
 }
---
+++ src/main/kotlin/br/com/usedrip/banktransfers/service/CustonTransferService.kt
@@ -0,0 +1,15 @@
+package br.com.usedrip.banktransfers.service
+
+import br.com.usedrip.banktransfers.dto.TransferForm
+import br.com.usedrip.banktransfers.dto.TransfersDTO
+import br.com.usedrip.banktransfers.model.Account
+import br.com.usedrip.banktransfers.rules.ITransferRule
+import org.springframework.stereotype.Service
+import java.math.BigDecimal
+@Service
+class CustonTransferService(override var comission: BigDecimal = BigDecimal.valueOf(1.5),
+                            private val transferService: TransferService) : ITransferRule  {
+    override fun convert(transferForm: TransferForm, accountTo: Account, accountFor: Account): TransfersDTO {
+       return transferService.transfer(transferForm, comission, accountTo, accountFor, true)
+    }
+}
